a1e01dde0a3eaf8802f918102c70544b69162ba4
DSergio1989 <daniel.vm26@yahoo.com>
2018-06-27 17:05:54 -0300

    Update lemons.py
    empilhar limões, indicando quantos limões por camada

--- lemons.py
+++ lemons.py
@@ -3,22 +3,27 @@ n = 1
 resultado = 0
 
 # calcula o maximo de camadas com X limões
-while resultado < quantidade-1:
+while resultado < quantidade:
      c = (n*(n+1))/2 # calcula quantos limões tem na camada n
      resultado = resultado + c
      n = n + 1
-     camada = c + c
-    
+
 if quantidade - resultado <= 1:
+  n = n - 1
   camada = 1
   while quantidade > 0:
     c = (n*(n+1))/2
-    quantidade = quantidade - c
     
-    if quantidade > 0:
+    if quantidade - c >= 0:
+      quantidade = quantidade - c
       print ("camada ", camada, "com", c, "limões" )
       n = n - 1
       camada = camada + 1
+    elif quantidade - c < 0:
+      if (c - quantidade) == 1:
+        print ("camada ", camada, "com", quantidade, "limões" )
+        quantidade = quantidade - c
+      else:
+        n = n - 1
     else:
-      print ("Camada", camada, "com", quantidade+c, "limões")
-      camada = camada + 1
+      print ()     
